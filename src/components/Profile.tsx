d5e37b31e727b21edaba410f04d12386ac7d52db
Gabriel Naves <gabri.navess@gmail.com>
2021-02-24 20:10:27 -0300

    Aula 3 terminada

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,9 +3,9 @@ import styles from '../styles/components/Profile.module.css'
 export function Profile() {
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+      <img src="https://github.com/gabrielnaves.png" alt="Gabriel Naves" />
       <div>
-        <strong>Diego Fernandes</strong>
+        <strong>Gabriel Naves</strong>
         <p>
           <img src="icons/level.svg" alt="" />
           Level 1
